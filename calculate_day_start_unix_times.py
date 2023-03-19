ff65a4dc46037788951278a3f230fb5192a67fed
Justin Liu <justinliu1308@gmail.com>
2023-03-18 23:51:34 -0400

    initial commit

---
+++ calculate_day_start_unix_times.py
@@ -0,0 +1,27 @@
+import time
+from datetime import datetime, timedelta
+
+
+current_unix_time = int(time.time())                            # Get current Unix time
+current_datetime = datetime.fromtimestamp(current_unix_time)    # Convert Unix time to datetime object
+print(f"\nThe current local unix time is: {current_unix_time}")
+print(f"The current local time is:      {current_datetime}\n")
+
+days = 5        # days = how many days in the future you want unix start times for
+unix_start_times = []
+human_start_times = []
+for i in range(days):
+    next_day_datetime = current_datetime + timedelta(days=i+1)  # Add days to datetime object
+    next_day_datetime = next_day_datetime.replace(hour=0, minute=0, second=0, microsecond=0)    # Set time component to 00:00:00
+    next_day_unix_time = int(next_day_datetime.timestamp())     # Convert datetime object back to Unix time for the next day
+    unix_start_times.append(next_day_unix_time)
+    dt = datetime.fromtimestamp(next_day_unix_time)             # Convert new day Unix time to datetime object
+    date_string = dt.strftime("%Y-%m-%d %H:%M:%S")              # Convert datetime object to human-readable string
+    human_start_times.append(date_string)
+print(f"Below is a list of the local unix times for the start of each day for the next {days} days:")
+print("   ", unix_start_times)
+print(f"Below is a list of the same times in human readable format (ISO 8601):")
+print("   ", human_start_times, "\n")
+
+
+
---
+++ convert_unix_time.py
@@ -0,0 +1,12 @@
+import time
+from datetime import datetime
+
+
+# Converts local unix time to human readable format (ISO 8601): YYYY-MM-DD HH:MM:SS
+current_unix_time = int(time.time())                            # Get current Unix time
+current_datetime = datetime.fromtimestamp(current_unix_time)    # Convert Unix time to datetime object
+date_string = current_datetime.strftime("%Y-%m-%d %H:%M:%S")    # Convert datetime object to human-readable string
+
+# Print output
+print(f"\nCurrent unix time: {current_unix_time}")
+print(f"Current time:      {current_datetime}\n")
